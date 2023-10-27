0117a3fdb86cb072ee9b3fb45ae6685153e19b36
jadecobra <jakeitegsy@yahoo.com>
2023-10-26 23:57:49 -0500

    remove dependency

--- source/conf.py
+++ source/conf.py
@@ -1,23 +1,9 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = "pumping python"
 copyright = "2023, JadeCobra LLC"
 author = "Jacob Itegboje"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = ["m2r"]
-source_suffix = [".rst", ".rst"]
 templates_path = ["_templates"]
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
