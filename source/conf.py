d5b4d70e1bda9421f8587e5ff6455cac8e0f78af
jadecobra <jakeitegsy@yahoo.com>
2024-01-20 06:57:06 -0600

    update

--- source/conf.py
+++ source/conf.py
@@ -8,8 +8,9 @@ exclude_patterns = ["learning_models.rst", "code/code_tree.rst",]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_logo = "_static/DRY_html_logo.png"
+html_favicon = "_static/dry_favicon_16x16.png"
 # html_theme = "alabaster"
 html_theme = 'press'
 html_static_path = ["_static"]
 extensions = ["sphinx.ext.autosectionlabel"]
-html_logo = '_static/DRY_html_logo.png'
