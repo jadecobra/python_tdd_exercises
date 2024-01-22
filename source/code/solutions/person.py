49df2093f338e012b8104c6aebe833a10a958e19
jadecobra <jakeitegsy@yahoo.com>
2024-01-21 21:10:35 -0600

    edit

--- source/code/solutions/person.py
+++ source/code/solutions/person.py
@@ -1,12 +1,10 @@
 import datetime
 
-def factory(
-    first_name=None, last_name="last_name",
-    year_of_birth=None, sex="M"
-):
+
+def factory(first_name=None, last_name="last_name", year_of_birth=None, sex="M"):
     return {
-        'first_name': first_name,
-        'last_name': last_name,
-        'sex': sex,
-        'age': datetime.datetime.now().year - year_of_birth,
+        "first_name": first_name,
+        "last_name": last_name,
+        "sex": sex,
+        "age": datetime.datetime.now().year - year_of_birth,
     }
--- source/code/tests/test_person.py
+++ source/code/tests/test_person.py
@@ -2,33 +2,18 @@ import datetime
 import person
 import unittest
 
+
 def this_year():
     return datetime.datetime.now().year
 
 
 class TestPersonFactory(unittest.TestCase):
-
     def test_person_factory(self):
-        self.assertEqual(
-            person.factory(
-                first_name="sibling",
-                last_name="last_name",
-                year_of_birth=this_year(),
-                sex="F"
-            ),
-            {
-                "first_name": "sibling",
-                "last_name": "last_name",
-                "sex": "F",
-                "age": this_year() - this_year()
-            }
-        )
+        first_name = "baby"
+        last_name = "last_name"
+        sex = "F"
+        year_of_birth = this_year()
 
-    def test_person_factory_takes_in_variable_inputs(self):
-        first_name = "john"
-        last_name = "doe"
-        sex = "M"
-        year_of_birth = 1942
         self.assertEqual(
             person.factory(
                 first_name=first_name,
@@ -41,30 +26,49 @@ class TestPersonFactory(unittest.TestCase):
                 "last_name": last_name,
                 "sex": sex,
                 "age": this_year() - year_of_birth,
-            }
+            },
+        )
+
+    def test_person_factory_takes_in_variable_inputs(self):
+        first_name = "john"
+        last_name = "doe"
+        sex = "M"
+        year_of_birth = 1983
+
+        self.assertEqual(
+            person.factory(
+                first_name=first_name,
+                last_name=last_name,
+                year_of_birth=year_of_birth,
+                sex=sex,
+            ),
+            {
+                "first_name": first_name,
+                "last_name": last_name,
+                "sex": sex,
+                "age": this_year() - year_of_birth,
+            },
         )
 
     def test_person_factory_with_default_keyword_arguments(self):
         first_name = "child_a"
         sex = "M"
         year_of_birth = 2014
+
         self.assertEqual(
-            person.factory(
-                first_name=first_name,
-                year_of_birth=year_of_birth,
-                sex=sex,
-            ),
+            person.factory(first_name=first_name, year_of_birth=year_of_birth, sex=sex),
             {
                 "first_name": first_name,
                 "last_name": "last_name",
                 "sex": sex,
-                "age": this_year() - year_of_birth
-            }
+                "age": this_year() - year_of_birth,
+            },
         )
 
-    def test_person_factory_with_sex_default_keyword_arguments(self):
+    def test_person_factory_with_sex_default_keyword_argument(self):
         first_name = "person"
         year_of_birth = 1900
+
         self.assertEqual(
             person.factory(
                 first_name=first_name,
@@ -74,8 +78,8 @@ class TestPersonFactory(unittest.TestCase):
                 "first_name": first_name,
                 "last_name": "last_name",
                 "age": this_year() - year_of_birth,
-                "sex": "M"
-            }
+                "sex": "M",
+            },
         )
 
 # Exceptions Encountered
