65e61626f0a4bbb5b8aed067c8c237e48133bcbc
jadecobra <jakeitegsy@yahoo.com>
2024-02-16 05:45:57 -0600

    refactor

--- source/code/tests/test_sleep_duration.py
+++ source/code/tests/test_sleep_duration.py
@@ -3,7 +3,7 @@ import random
 import sleep_duration
 import unittest
 
-def random_timestamp(date):
+def get_random_timestamp(date):
     return f'{date} {random.randint(0, 23):02}:{random.randint(0, 59):02}'
 
 
@@ -134,19 +134,19 @@ class TestSleepDuration(unittest.TestCase):
     def test_datetime_objects(self):
         self.assertEqual(
             datetime.datetime.strptime(
-                "21/11/06 16:30",
-                "%d/%m/%y %H:%M"
+                '21/11/06 16:30',
+                '%d/%m/%y %H:%M'
             ),
             datetime.datetime(2006, 11, 21, 16, 30)
         )
 
     def test_subtracting_datetime_objects(self):
-        pattern = "%d/%m/%y %H:%M"
+        pattern = '%d/%m/%y %H:%M'
         sleep_time = datetime.datetime.strptime(
-            "21/11/06 16:30", pattern
+            '21/11/06 16:30', pattern
         )
         wake_time = datetime.datetime.strptime(
-            "21/11/06 17:30", pattern
+            '21/11/06 17:30', pattern
         )
         self.assertEqual(
             wake_time-sleep_time,
@@ -159,15 +159,31 @@ class TestSleepDuration(unittest.TestCase):
             '2:07:34'
         )
 
+    def test_duration_w_earlier_wake_than_sleep_time(self):
+        wake_time = '21/12/12 01:00'
+        sleep_time = '21/12/12 02:00'
+
+        pattern = '%d/%m/%y %H:%M'
+
+        with self.assertRaisesRegex(
+            ValueError,
+            f'wake_time: {datetime.datetime.strptime(wake_time, pattern)} is earlier '
+            f'than sleep_time: {datetime.datetime.strptime(sleep_time, pattern)}'
+        ):
+            sleep_duration.duration(
+                wake_time=wake_time,
+                sleep_time=sleep_time
+            )
+
     def test_duration_w_date_and_time(self):
         pattern = '%d/%m/%y %H:%M'
 
-        wake_time = random_timestamp('31/12/99')
+        wake_time = get_random_timestamp('31/12/99')
         wake_datetime_object = datetime.datetime.strptime(
             wake_time, pattern
         )
 
-        sleep_time = random_timestamp('30/12/99')
+        sleep_time = get_random_timestamp('31/12/99')
         sleep_datetime_object = datetime.datetime.strptime(
             sleep_time, pattern
         )
@@ -196,10 +212,10 @@ class TestSleepDuration(unittest.TestCase):
                     sleep_time=sleep_time
                 )
 
-
 # Exceptions Encountered
 # AssertionError
 # NameError
 # AttributeError
 # TypeError
-# SyntaxError
+# SyntaxEror
+# ValueError
