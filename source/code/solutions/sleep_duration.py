af587f8c44ba74c917f4775bc87861ecc3819732
jadecobra <jakeitegsy@yahoo.com>
2024-04-06 03:59:37 -0500

    format

--- source/code/solutions/sleep_duration.py
+++ source/code/solutions/sleep_duration.py
@@ -2,7 +2,9 @@ import datetime
 
 
 def get_datetime_object(timestamp):
-    return datetime.datetime.strptime(timestamp, "%d/%m/%y %H:%M")
+    return datetime.datetime.strptime(
+        timestamp, "%d/%m/%y %H:%M"
+    )
 
 
 def duration(wake_time=None, sleep_time=None):
