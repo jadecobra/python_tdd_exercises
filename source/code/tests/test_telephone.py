9321f9467daf3807919c0b10520c43d579c78b83
jadecobra <jakeitegsy@yahoo.com>
2024-07-18 20:30:49 -0500

    undo

--- source/code/tests/test_telephone.py
+++ source/code/tests/test_telephone.py
@@ -4,24 +4,60 @@ import unittest
 
 class TestTelephone(unittest.TestCase):
 
-    def test_passing_values(self):
-        for value in (
-            'hello',
-            'yes',
-            None,
-            1234,
-            1.234,
-            (1, 2, 3, 'n'),
-            [1, 2, 3, 'n'],
-            {
+    def test_passing_a_string(self):
+        self.assertEqual(
+            src.telephone.text('hello'),
+            "I received this message: hello"
+        )
+        self.assertEqual(
+            src.telephone.text('yes'),
+            "I received this message: yes"
+        )
+
+    def test_passing_None(self):
+        self.assertEqual(
+            src.telephone.text(None),
+            "I received this message: None"
+        )
+
+    def test_passing_a_class(self):
+        self.assertEqual(
+            src.telephone.text(bool),
+            "I received this message: <class 'bool'>"
+        )
+
+    def test_passing_an_integer(self):
+        self.assertEqual(
+            src.telephone.text(1234),
+            "I received this message: 1234"
+        )
+
+    def test_passing_a_float(self):
+        self.assertEqual(
+            src.telephone.text(1.234),
+            "I received this message: 1.234"
+        )
+
+    def test_passing_a_tuple(self):
+        self.assertEqual(
+            src.telephone.text((1, 2, 3, 'n')),
+            "I received this message: (1, 2, 3, 'n')"
+        )
+
+    def test_passing_a_list(self):
+        self.assertEqual(
+            src.telephone.text([1, 2, 3, 'n']),
+            "I received this message: [1, 2, 3, 'n']"
+        )
+
+    def test_passing_a_dictionary(self):
+        self.assertEqual(
+            src.telephone.text({
                 'key1': 'value1',
                 'keyN': 'valueN'
-            },
-        ):
-            self.assertEqual(
-                src.telephone.text(value),
-                f"I received this message: {value}"
-            )
+            }),
+            "I received this message: {'key1': 'value1', 'keyN': 'valueN'}"
+        )
 
 
 # Exceptions Encountered
