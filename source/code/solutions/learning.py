caee78a6c1a9d06fc73cb27ca1f8ba370cac4bf6
jadecobra <jakeitegsy@yahoo.com>
2024-09-04 01:30:32 -0500

    edit

--- source/code/solutions/learning.py
+++ source/code/solutions/learning.py
@@ -3,10 +3,3 @@ def model(expectations, reality):
         return expectations + 1
     else:
         return reality + 1
-
-
-def model(expectations, reality):
-    if expectations <= reality:
-        return reality + 1
-    else:
-        return expectations + 1
