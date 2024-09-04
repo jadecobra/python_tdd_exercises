c2ab21bba7e1ec0918927274a7a994485898293f
jadecobra <jakeitegsy@yahoo.com>
2024-09-04 00:46:00 -0500

    refactor

--- source/code/tests/test_learning_models.py
+++ source/code/tests/test_learning_models.py
@@ -5,36 +5,32 @@ import unittest
 class TestInfiniteLearningModel(unittest.TestCase):
 
     def test_learning_model_when_expectations_are_less_than_reality(self):
-        '''When expectations are less than reality,
-        increase expectations until they are greater than reality'''
-
         reality = 1
         expectations = reality - 1
 
         self.assertGreater(
             learning.model(expectations, reality),
-            reality
+            reality,
+            '''When expectations are less than reality, increase expectations until they are greater than reality'''
         )
 
     def test_learning_model_when_expectations_equal_reality(self):
-        '''When expectations equal reality, increase expectations'''
-
         reality = 1
         expectations = reality
 
         self.assertGreater(
             learning.model(expectations, reality),
-            expectations
+            expectations,
+            '''When expectations equal reality, increase expectations'''
         )
 
     def test_learning_model_when_expectations_are_greater_than_reality(self):
-        '''When expectations are greater than reality,
-        increase reality until it is greater than expectations'''
 
         reality = 0
         expectations = reality + 1
 
         self.assertGreater(
             learning.model(expectations, reality),
-            expectations
+            expectations,
+            '''When expectations are greater than reality, increase reality until it is greater than expectations'''
         )
