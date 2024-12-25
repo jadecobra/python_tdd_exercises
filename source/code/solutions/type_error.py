2b27d01b1d763fbdfed466ffee35562d95d26890
jadecobra <jakeitegsy@yahoo.com>
2024-12-25 01:03:07 -0600

    edit

--- source/code/solutions/type_error.py
+++ source/code/solutions/type_error.py
@@ -1,3 +1,23 @@
+def none():
+    return None
+
+
+def true():
+    return True
+
+
+def false():
+    return False
+
+
+def a_list():
+    return [1, 2, 3, 4]
+
+
+def a_dictionary():
+    return {'key': 'value'}
+
+
 def function_00():
     return None
 
--- source/code/tests/test_type_error.py
+++ source/code/tests/test_type_error.py
@@ -4,11 +4,16 @@ import src.type_error
 
 class TestTypeError(unittest.TestCase):
 
+    def test_type_error_w_callables(self):
+        src.type_error.none()
+        src.type_error.true()
+        src.type_error.false()
+
     def test_type_error_w_function_signatures(self):
-        src.type_error.function_00()
-        src.type_error.function_01('a')
-        src.type_error.function_02('a', 'b')
-        src.type_error.function_03('a', 'b', 'c')
+        src.type_error.function_00('a')
+        src.type_error.function_01('a', 'b')
+        src.type_error.function_02('a', 'b', 'c')
+        src.type_error.function_03('a', 'b', 'c', 'd')
 
 
 # Exceptions Encountered
