db79e4ab81e21b7aa1ccc2553b9d23337308289e
jadecobra <jakeitegsy@yahoo.com>
2025-01-28 14:50:42 -0600

    revert

--- source/code/tests/test_none.py
+++ source/code/tests/test_none.py
@@ -7,27 +7,60 @@ class TestNone(unittest.TestCase):
         self.assertIsNone(None)
 
     def test_is_none_a_boolean(self):
+        self.assertIsNotNone(False)
+        self.assertIsNotNone(True)
+        self.assertIsInstance(False, bool)
+        self.assertIsInstance(True, bool)
         self.assertNotIsInstance(None, bool)
 
     def test_is_none_an_integer(self):
+        self.assertIsNotNone(-1)
+        self.assertIsNotNone(0)
+        self.assertIsNotNone(1)
+        self.assertIsInstance(-1, int)
+        self.assertIsInstance(0, int)
+        self.assertIsInstance(1, int)
         self.assertNotIsInstance(None, int)
 
     def test_is_none_a_float(self):
+        self.assertIsNotNone(-1.2)
+        self.assertIsNotNone(3.4)
+        self.assertIsInstance(-1.2, float)
+        self.assertIsInstance(3.4, float)
         self.assertNotIsInstance(None, float)
 
     def test_is_none_a_string(self):
+        self.assertIsNotNone('')
+        self.assertIsNotNone("text")
+        self.assertIsInstance('', str)
+        self.assertIsInstance("text", str)
         self.assertNotIsInstance(None, str)
 
     def test_is_none_a_tuple(self):
+        self.assertIsNotNone(())
+        self.assertIsNotNone((1, 2, 3, 'n'))
+        self.assertIsInstance((), tuple)
+        self.assertIsInstance((1, 2, 3, 'n'), tuple)
         self.assertNotIsInstance(None, tuple)
 
     def test_is_none_a_list(self):
+        self.assertIsNotNone([])
+        self.assertIsNotNone([1, 2, 3, 'n'])
+        self.assertIsInstance([], list)
+        self.assertIsInstance([1, 2, 3, 'n'], list)
         self.assertNotIsInstance(None, list)
 
     def test_is_none_a_set(self):
+        self.assertIsNotNone(set())
+        self.assertIsNotNone({1, 2, 3, 'n'})
+        self.assertIsInstance({1, 2, 3, 'n'}, set)
         self.assertNotIsInstance(None, set)
 
     def test_is_none_a_dictionary(self):
+        self.assertIsNotNone(dict())
+        self.assertIsNotNone({'key': 'value'})
+        self.assertIsInstance({}, dict)
+        self.assertIsInstance({'key': 'value'}, dict)
         self.assertNotIsInstance(None, dict)
 
 
