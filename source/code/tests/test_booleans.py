bf252be9a5e58cb94efe6e61f5106ec2b6226a46
jadecobra <jakeitegsy@yahoo.com>
2025-02-25 16:28:44 -0600

    update test

--- source/code/tests/test_booleans.py
+++ source/code/tests/test_booleans.py
@@ -9,11 +9,11 @@ class TestBooleans(unittest.TestCase):
         self.assertFalse(None)
         self.assertFalse(0)
         self.assertFalse(0.0)
-        self.assertFalse('')
-        self.assertFalse(())
-        self.assertFalse([])
+        self.assertFalse(str())
+        self.assertFalse(tuple())
+        self.assertFalse(list())
         self.assertFalse(set())
-        self.assertFalse({})
+        self.assertFalse(dict())
 
     def test_what_is_true(self):
         self.assertIsInstance(True, bool)
@@ -27,6 +27,29 @@ class TestBooleans(unittest.TestCase):
         self.assertTrue([1, 2, 3, 'n'])
         self.assertTrue({1, 2, 3, 'n'})
         self.assertTrue({'key': 'value'})
+
+
+# NOTES
+# a dictionary with things is true
+# a set with things is true
+# a list with things is true
+# a tuple with things is true
+# a string with things is true
+# positive and negative numbers are true
+# True is true
+# True is not false
+# True is a boolean
+# the empty dictionary is false
+# the empty set is false
+# the empty list is false
+# the empty tuple is false
+# the empty string is false
+# 0 is false
+# None is false
+# False is false
+# False is not true
+# False is a boolean
+
 
 # Exceptions Encountered
 # AssertionError
