c2d1e78622d22983a110f4d1462aa4884b351b62
jadecobra <jakeitegsy@yahoo.com>
2025-05-28 23:50:03 -0500

    refactor

--- source/code/tests/test_lists.py
+++ source/code/tests/test_lists.py
@@ -9,66 +9,11 @@ class TestLists(unittest.TestCase):
     def test_make_a_list_w_square_brackets(self):
         self.assertEqual([0, 1, 2, 3], list((0, 1, 2, 3)))
 
-    def test_make_a_list_from_an_iterable(self):
+    def test_make_a_list_w_an_iterable(self):
         iterable = range(4)
         self.assertEqual(list(iterable), [0, 1, 2, 3])
 
-    def test_add_to_a_list(self):
-        a_list = [0, 1, 2, 3]
-        self.assertIsNone(a_list.append(4))
-        self.assertEqual(a_list, [0, 1, 2, 3, 4])
-
-    def test_remove_from_a_list(self):
-        a_list = [0, 1, 2, 3]
-        self.assertIsNone(a_list.remove(2))
-        self.assertEqual(a_list, [0, 1, 3])
-
-    def test_remove_from_a_list_when_item_occurs_multiple_times(self):
-        a_list = [0, 2, 1, 2, 3, 2]
-        self.assertIsNone(a_list.remove(2))
-        self.assertEqual(a_list, [0, 1, 2, 3, 2])
-
-    def test_remove_from_list_when_item_does_not_exist(self):
-        a_list = [0, 1, 2, 3]
-        with self.assertRaises(ValueError):
-            a_list.remove(4)
-
-    def test_remove_last_item_from_a_list(self):
-        a_list = [0, 1, 2, 3]
-        self.assertEqual(a_list.pop(), 3)
-        self.assertEqual(a_list, [0, 1, 2])
-
-    def test_remove_last_item_from_empty_list(self):
-        with self.assertRaises(IndexError):
-            [].pop()
-
-    def test_view_items_in_a_list(self):
-        a_list = ['1st', '2nd', '3rd', '... last']
-        self.assertEqual(a_list[0], '1st')
-        self.assertEqual(a_list[-4], '1st')
-        self.assertEqual(a_list[2], '3rd')
-        self.assertEqual(a_list[-2], '3rd')
-        self.assertEqual(a_list[1], '2nd')
-        self.assertEqual(a_list[-3], '2nd')
-        self.assertEqual(a_list[3], '... last')
-        self.assertEqual(a_list[-1], '... last')
-
-    def test_view_parts_of_a_list(self):
-        a_list = ['1st', '2nd', '3rd', '... last']
-        self.assertEqual(a_list[0:2], ['1st', '2nd'])
-        self.assertEqual(a_list[0:3], ['1st', '2nd', '3rd'])
-        self.assertEqual(a_list[1:3], ['2nd', '3rd'])
-        self.assertEqual(a_list[1:4], ['2nd', '3rd', '... last'])
-
-    def test_index_error(self):
-        a_list = ['a', 'b', 'c', 'd']
-
-        with self.assertRaises(IndexError):
-            a_list[4]
-        with self.assertRaises(IndexError):
-            a_list[-5]
-
-    def test_attributes_and_methods_of_a_list(self):
+    def test_attributes_and_methods_of_lists(self):
         self.maxDiff = None
         self.assertEqual(
             dir(list),
@@ -123,6 +68,97 @@ class TestLists(unittest.TestCase):
                 'sort'
             ]
         )
+
+    def test_append_adds_to_a_list(self):
+        a_list = [0, 1, 2, 3]
+        self.assertIsNone(a_list.append(4))
+        self.assertEqual(a_list, [0, 1, 2, 3, 4])
+
+    def test_clear_empties_a_list(self):
+        a_list = [0, 1, 2, 3]
+        self.assertIsNone(a_list.clear())
+        self.assertEqual(a_list, [])
+
+    def test_copy_a_list(self):
+        a_list = [0, 1, 2, 3]
+        self.assertEqual(a_list.copy(), [0, 1, 2, 3])
+        self.assertEqual(a_list, [0, 1, 2, 3])
+
+    def test_count_occurrence_of_item_in_a_list(self):
+        a_list = [0, 2, 1, 2, 3, 2]
+        self.assertEqual(a_list.count(0), 1)
+        self.assertEqual(a_list.count(2), 3)
+        self.assertEqual(a_list.count(9), 0)
+
+    def test_extend_makes_a_list_longer(self):
+        a_list = [0, 1, 2, 3]
+        self.assertIsNone(a_list.extend((4, 5, 6, 7)))
+        self.assertEqual(a_list, [0, 1, 2, 3, 4, 5, 6, 7])
+
+    def test_index_returns_position_of_item_in_a_list(self):
+        a_list = ['1st', '2nd', '3rd', '... last']
+        self.assertEqual(a_list.index('1st'), 0)
+        self.assertEqual(a_list.index('3rd'), 2)
+        self.assertEqual(a_list.index('2nd'), 1)
+        self.assertEqual(a_list.index('... last'), 3)
+
+        with self.assertRaises(ValueError):
+            a_list.index(0)
+
+    def test_insert_places_item_at_given_index_in_a_list(self):
+        a_list = [0, 1, 2, 3]
+        self.assertIsNone(a_list.insert(0, 1))
+        self.assertEqual(a_list, [1, 0, 1, 2, 3])
+
+    def test_pop_removes_and_returns_the_last_item_in_a_list(self):
+        a_list = [0, 1, 2, 3]
+        self.assertEqual(a_list.pop(), 3)
+        self.assertEqual(a_list, [0, 1, 2])
+
+    def test_remove_first_occurrence_of_item_from_a_list(self):
+        a_list = [0, 2, 1, 2, 3, 2]
+        self.assertIsNone(a_list.remove(2))
+        self.assertEqual(a_list, [0, 1, 2, 3, 2])
+
+        with self.assertRaises(ValueError):
+            a_list.remove(4)
+
+    def test_reverse_a_list(self):
+        a_list = [0, 1, 2, 3]
+        self.assertIsNone(a_list.reverse())
+        self.assertEqual(a_list, [3, 2, 1, 0])
+
+    def test_sort_a_list(self):
+        a_list = [0, 2, 1, 2, 3, 2]
+        self.assertIsNone(a_list.sort())
+        self.assertEqual(a_list, [0, 1, 2, 2, 2, 3])
+
+    def test_view_items_in_a_list(self):
+        a_list = ['1st', '2nd', '3rd', '... last']
+        self.assertEqual(a_list[0], '1st')
+        self.assertEqual(a_list[-4], '1st')
+        self.assertEqual(a_list[2], '3rd')
+        self.assertEqual(a_list[-2], '3rd')
+        self.assertEqual(a_list[1], '2nd')
+        self.assertEqual(a_list[-3], '2nd')
+        self.assertEqual(a_list[3], '... last')
+        self.assertEqual(a_list[-1], '... last')
+
+    def test_view_parts_of_a_list(self):
+        a_list = ['1st', '2nd', '3rd', '... last']
+        self.assertEqual(a_list[0:2], ['1st', '2nd'])
+        self.assertEqual(a_list[0:3], ['1st', '2nd', '3rd'])
+        self.assertEqual(a_list[1:4], ['2nd', '3rd', '... last'])
+        self.assertEqual(a_list[1:3], ['2nd', '3rd'])
+
+    def test_index_error(self):
+        a_list = ['a', 'b', 'c', 'd']
+
+        with self.assertRaises(IndexError):
+            a_list[4]
+        with self.assertRaises(IndexError):
+            a_list[-5]
+
 
 
 # Exceptions Encountered
