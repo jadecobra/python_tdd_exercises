b70332ec50ba49cf5ed2dc24f62686aced2240cd
jadecobra <jakeitegsy@yahoo.com>
2025-08-01 21:02:06 -0500

    edit

--- source/code/tests/test_functions.py
+++ source/code/tests/test_functions.py
@@ -36,7 +36,7 @@ class TestFunctions(unittest.TestCase):
     def test_functions_w_keyword_arguments(self):
         self.assertEqual(
             src.functions.w_keyword_arguments(
-                x='first', y='second'
+                x='first', y='second',
             ),
             ('first', 'second')
         )
@@ -61,9 +61,7 @@ class TestFunctions(unittest.TestCase):
 
     def test_functions_w_default_arguments(self):
         self.assertEqual(
-            src.functions.w_default_arguments(
-                'jane',
-            ),
+            src.functions.w_default_arguments('jane'),
             ('jane', 'doe')
         )
 
@@ -77,17 +75,28 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(
             src.functions.w_unknown_arguments(
                 None, bool, int, float, str, tuple, list, set, dict,
-                none=None, a_boolean=bool, an_integer=int, a_float=float,
-                a_string=str, a_list=list, a_set=set, a_dictionary=dict,
+                none=None, a_boolean=bool, an_integer=int,
+                a_float=float, a_string=str, a_list=list,
+                a_set=set, a_dictionary=dict
             ),
             (
-                (None, bool, int, float, str, tuple, list, set, dict),
+                (
+                    None, bool, int, float, str, tuple, list, set, dict
+                ),
                 dict(
                     a_boolean=bool, a_dictionary=dict, a_float=float,
                     a_list=list, a_set=set, a_string=str, an_integer=int,
-                    none=None,
+                    none=None
                 )
             )
+        )
+        self.assertEqual(
+            src.functions.w_unknown_arguments(0, 1, 2, 3),
+            ((0, 1, 2, 3), {})
+        )
+        self.assertEqual(
+            src.functions.w_unknown_arguments(a=5, b=6, c=7, d=8),
+            ((), {'a': 5, 'b': 6, 'c': 7, 'd': 8})
         )
 
 
