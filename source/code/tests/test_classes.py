4ad6c6796597f7daeb856fa3046c7e21ea3b8545
jadecobra <jakeitegsy@yahoo.com>
2025-08-19 06:35:36 -0500

    edit

--- source/code/tests/test_classes.py
+++ source/code/tests/test_classes.py
@@ -121,9 +121,39 @@ class TestPerson(unittest.TestCase):
 
     def test_attributes_and_methods_of_classes(self):
         self.assertEqual(
-            dir(src.class.Person),
+            dir(src.person.Person),
             [
-
+                '__class__',
+                '__delattr__',
+                '__dict__',
+                '__dir__',
+                '__doc__',
+                '__eq__',
+                '__firstlineno__',
+                '__format__',
+                '__ge__',
+                '__getattribute__',
+                '__getstate__',
+                '__gt__',
+                '__hash__',
+                '__init__',
+                '__init_subclass__',
+                '__le__',
+                '__lt__',
+                '__module__',
+                '__ne__',
+                '__new__',
+                '__reduce__',
+                '__reduce_ex__',
+                '__repr__',
+                '__setattr__',
+                '__sizeof__',
+                '__static_attributes__',
+                '__str__',
+                '__subclasshook__',
+                '__weakref__',
+                'get_age',
+                'introduce'
             ]
         )
 
