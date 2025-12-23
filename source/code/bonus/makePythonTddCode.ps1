771e4fce4c45f84eee30dee1ac617bf1902a9bf3
jadecobra <jakeitegsy@yahoo.com>
2025-12-22 19:16:49 -0600

    add code to makePythonTdd

---
+++ source/code/bonus/makePythonTddCode.ps1
@@ -0,0 +1,28 @@
+$PROJECT_NAME=$args[0]
+mkdir $PROJECT_NAME
+cd $PROJECT_NAME
+mkdir src
+New-Item "src/$PROJECT_NAME.py"
+mkdir tests
+New-Item tests/__init__.py
+
+"import unittest
+
+
+class Test$($PROJECT_NAME)(unittest.TestCase):
+
+    def test_failure(self):
+        self.assertFalse(True)
+
+
+# Exceptions seen
+# AssertionError
+" | Out-File "tests/test_$PROJECT_NAME.py" -Encoding UTF8
+code tests/test_$PROJECT_NAME.py
+
+python -m venv .venv
+.venv/scripts/activate.ps1
+python -m pip install --upgrade pip
+"pytest-watch" | Out-File requirements.txt -Encoding UTF8
+python -m pip install --requirement requirements.txt
+pytest-watch
