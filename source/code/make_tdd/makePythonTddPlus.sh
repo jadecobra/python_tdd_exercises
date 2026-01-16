9c73a85738939b04bb7a41051699d1d0248e1614
jadecobra <jakeitegsy@yahoo.com>
2026-01-15 21:48:23 -0600

    add variants

---
+++ source/code/make_tdd/makePythonTddPlus.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+PROJECT_NAME=$1
+mkdir $PROJECT_NAME
+cd $PROJECT_NAME
+mkdir src
+touch src/$PROJECT_NAME.py
+mkdir tests
+touch tests/__init__.py
+
+echo "import unittest
+
+
+class Test$PROJECT_NAME(unittest.TestCase):
+
+    def test_failure(self):
+        self.assertFalse(True)
+
+
+# Exceptions seen
+# AssertionError
+" > tests/test_$PROJECT_NAME.py
+
+code tests/test_$PROJECT_NAME.py
+code src/$PROJECT_NAME.py
+
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install --upgrade pip
+echo "pytest-watch" > requirements.txt
+python3 -m pip install --requirement requirements.txt
+pytest-watch
