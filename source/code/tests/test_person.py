211a2a4d6da610fd52a8cdaff8a2abcf89a4db19
jadecobra <jakeitegsy@yahoo.com>
2026-01-20 18:33:06 -0600

    edit

--- source/code/tests/test_person.py
+++ source/code/tests/test_person.py
@@ -11,30 +11,29 @@ def this_year():
 class TestPerson(unittest.TestCase):
 
     def setUp(self):
-        self.first_name = random.choice((
-            'jane', 'joe', 'john', 'person',
-        ))
         self.year_of_birth = random.randint(
             this_year()-120, this_year()
         )
+        self.first_name = random.choice((
+            'jane', 'joe', 'john', 'person',
+        ))
 
     def test_factory_takes_keyword_arguments(self):
-        last_name = random.choice((
-            'doe', 'smith', 'blow', 'public',
-        ))
-        sex = random.choice(('F', 'M'))
+        keyword_arguments = dict(
+            first_name=self.first_name,
+            last_name=random.choice((
+                'doe', 'smith', 'blow', 'public',
+            )),
+            sex=random.choice(('F', 'M')),
+        )
 
         self.assertEqual(
             src.person.factory(
-                first_name=self.first_name,
-                last_name=last_name,
-                sex=sex,
+                **keyword_arguments,
                 year_of_birth=self.year_of_birth,
             ),
             dict(
-                first_name=self.first_name,
-                last_name=last_name,
-                sex=sex,
+                **keyword_arguments,
                 age=this_year()-self.year_of_birth,
             )
         )
