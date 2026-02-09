dc944276a1dafcc0d3b271a73e966b56d0951c0c
jadecobra <jakeitegsy@yahoo.com>
2026-02-09 03:05:05 -0600

    edit

--- source/code/truth_table/tests/test_binary_ii.py
+++ source/code/truth_table/tests/test_binary_ii.py
@@ -4,29 +4,62 @@ import unittest
 
 class TestBinaryOperations(unittest.TestCase):
 
+
     def test_contradiction(self):
-        self.assertFalse(src.truth_table.contradiction(True, True))
-        self.assertFalse(src.truth_table.contradiction(True, False))
-        self.assertFalse(src.truth_table.contradiction(False, True))
-        self.assertFalse(src.truth_table.contradiction(False, False))
+        self.assertFalse(
+            src.truth_table.contradiction(True, True)
+        )
+        self.assertFalse(
+            src.truth_table.contradiction(True, False)
+        )
+        self.assertFalse(
+            src.truth_table.contradiction(False, True)
+        )
+        self.assertFalse(
+            src.truth_table.contradiction(False, False)
+        )
 
     def test_logical_conjunction(self):
-        self.assertTrue(src.truth_table.logical_conjunction(True, True))
-        self.assertFalse(src.truth_table.logical_conjunction(True, False))
-        self.assertFalse(src.truth_table.logical_conjunction(False, True))
-        self.assertFalse(src.truth_table.logical_conjunction(False, False))
+        self.assertTrue(
+            src.truth_table.logical_conjunction(True, True)
+        )
+        self.assertFalse(
+            src.truth_table.logical_conjunction(True, False)
+        )
+        self.assertFalse(
+            src.truth_table.logical_conjunction(False, True)
+        )
+        self.assertFalse(
+            src.truth_table.logical_conjunction(False, False)
+        )
 
     def test_project_second(self):
-        self.assertTrue(src.truth_table.project_second(True, True))
-        self.assertFalse(src.truth_table.project_second(True, False))
-        self.assertTrue(src.truth_table.project_second(False, True))
-        self.assertFalse(src.truth_table.project_second(False, False))
+        self.assertTrue(
+            src.truth_table.project_second(True, True)
+        )
+        self.assertFalse(
+            src.truth_table.project_second(True, False)
+        )
+        self.assertTrue(
+            src.truth_table.project_second(False, True)
+        )
+        self.assertFalse(
+            src.truth_table.project_second(False, False)
+        )
 
     def test_converse_non_implication(self):
-        self.assertFalse(src.truth_table.converse_non_implication(True, True))
-        self.assertFalse(src.truth_table.converse_non_implication(True, False))
-        self.assertTrue(src.truth_table.converse_non_implication(False, True))
-        self.assertFalse(src.truth_table.converse_non_implication(False, False))
+        self.assertFalse(
+            src.truth_table.converse_non_implication(True, True)
+        )
+        self.assertFalse(
+            src.truth_table.converse_non_implication(True, False)
+        )
+        self.assertTrue(
+            src.truth_table.converse_non_implication(False, True)
+        )
+        self.assertFalse(
+            src.truth_table.converse_non_implication(False, False)
+        )
 
     def test_negate_first(self):
         self.assertFalse(src.truth_table.negate_first(True, True))
@@ -47,14 +80,22 @@ class TestBinaryOperations(unittest.TestCase):
         self.assertTrue(src.truth_table.tautology(False, False))
 
     def test_logical_disjunction(self):
-        self.assertTrue(src.truth_table.logical_disjunction(True, True))
-        self.assertTrue(src.truth_table.logical_disjunction(True, False))
-        self.assertTrue(src.truth_table.logical_disjunction(False, True))
-        self.assertFalse(src.truth_table.logical_disjunction(False, False))
+        self.assertTrue(
+            src.truth_table.logical_disjunction(True, True)
+        )
+        self.assertTrue(
+            src.truth_table.logical_disjunction(True, False)
+        )
+        self.assertTrue(
+            src.truth_table.logical_disjunction(False, True)
+        )
+        self.assertFalse(
+            src.truth_table.logical_disjunction(False, False)
+        )
 
 
 # Exceptions seen
-# AssertionError
 # AttributeError
 # TypeError
+# AssertionError
 # SyntaxError
