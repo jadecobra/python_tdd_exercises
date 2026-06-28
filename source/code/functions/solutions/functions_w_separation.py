98925c9c7d87640ac91c7385eca179021ae497d3
jakeitegsy <jakeitegsy@yahoo.com>
2026-06-28 18:34:31 -0500

    edit

--- source/code/functions/solutions/functions_w_separation.py
+++ source/code/functions/solutions/functions_w_separation.py
@@ -1,38 +1,7 @@
-def w_pass():
-    pass
-
-
-def w_return():
-    return
-
-
-def w_return_none():
-    return None
-
-
-def return_leaves_the_function():
-    return None
-    return 'only one way for this line to run'
-
-
-def constant():
-    return 'the same thing'
-
-
-def identity(the_input):
-    return the_input
-
-
-def positional_arguments(first_input, last_input):
-    return first_input, last_input
-
-
-def keyword_arguments(first_input, last_input):
-    return first_input, last_input
-
-
-def args_and_kwargs(first_input, last_input):
-    return first_input, last_input
+def unknown_number_of_arguments(
+    *positional, **keyword
+):
+    return positional, keyword
 
 
 def optional_arguments(
@@ -41,7 +10,37 @@ def optional_arguments(
     return first_input, last_input
 
 
-def unknown_number_of_arguments(
-    *positional_arguments, **keyword_arguments
-):
-    return positional_arguments, keyword_arguments
+def args_and_kwargs(argument, last_input):
+    return argument, last_input
+
+
+def keyword_arguments(first_input, last_input):
+    return first_input, last_input
+
+
+def positional_arguments(first_input, last_input):
+    return first_input, last_input
+
+
+def identity(argument):
+    return argument
+
+
+def constant():
+    return 'the same thing'
+
+
+def return_leaves_the_function():
+    return None
+
+
+def w_return_none():
+    return None
+
+
+def w_return():
+    return None
+
+
+def w_pass():
+    return None
